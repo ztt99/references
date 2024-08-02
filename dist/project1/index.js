c3214bcfc1395c06033e7b869c175e966997978c
张婷 <zhangting43@58.com>
2024-08-02 17:43:52 +0800

    init

---
+++ dist/project1/index.js
@@ -0,0 +1,4 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const project2_1 = require("../project2");
+console.log((0, project2_1.add)(1, 2));
---
+++ project1/index.ts
@@ -0,0 +1,3 @@
+import { add } from "../project2";
+
+console.log(add(1, 2));
